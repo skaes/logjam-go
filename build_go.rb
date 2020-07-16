aa7f17b9d8b2da367d91aeac68ab11b0983f5ea2
Stefan Kaes <stefan.kaes@xing.com>
2020-07-16 17:26:14 +0200

    update to golang version 1.14.5

--- build_go.rb
+++ build_go.rb
@@ -7,7 +7,7 @@ iteration i
 vendor "skaes@railsexpress.de"
 
 source "https://golang.org/dl/go#{version}.linux-amd64.tar.gz",
-       checksum: "aed845e4185a0b2a3c3d5e1d0a35491702c55889192bb9c30e67a3de6849c067"
+       checksum: "82a1b84f16858db03231eb201f90cce2a991078dda543879b87e738e2586854b"
 
 run "mv", "go", "/usr/local"
 run "ln", "-s", "/usr/local/go/bin/go", "/usr/local/bin/go"
